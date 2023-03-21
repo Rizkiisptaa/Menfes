230fd5a479237e31ebf3d36fd8ee52d6fbcbdbf6
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2023-03-21 23:47:11 +0700

    Update helper.py

--- plugins/helper/helper.py
+++ plugins/helper/helper.py
@@ -60,7 +60,7 @@ class Helper():
         link_2 = await self.bot.export_chat_invite_link(config.channel_2)
         link_3 = await self.bot.export_chat_invite_link(config.channel_3)
         markup = InlineKeyboardMarkup([
-            [InlineKeyboardButton('⚡ Channel base', url=link_1), InlineKeyboardButton('⚡ Channel base', url=link_2), InlineKeyboardButton('🏠 Group base', url=link_3)],
+            [InlineKeyboardButton('⚡ Channel base', url=link_1), InlineKeyboardButton('⚡ Channel base', url=link_2),|| InlineKeyboardButton('🏠 Group base', url=link_3)],
             [InlineKeyboardButton('♻️ Coba lagi', url=f'https://t.me/{self.bot.username}?start=start')]
         ])
         await self.bot.send_message(self.user_id, config.pesan_join, reply_to_message_id=self.message.id, reply_markup=markup)
